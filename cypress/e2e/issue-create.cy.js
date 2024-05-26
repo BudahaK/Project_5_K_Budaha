aa31edaf48cfd2b3bf96ee51d46e90f5c305ac93
BudahaK <buda.kris@gmail.com>
2024-05-26 15:55:34 +0300

    Resolved uncaught exception from Jira

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,34 +1,36 @@
-describe("Issue create", () => {
+describe('Issue create', () => {
   beforeEach(() => {
-    cy.visit("/");
+    // This will prevent Cypress from failing the test on uncaught exceptions
+    Cypress.on('uncaught:exception', (err) => {
+      return false;
+    });
+
+    cy.visit('/');
     cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .should('eq', `${Cypress.env('baseUrl')}project/board`)
       .then((url) => {
         // System will already open issue creating modal in beforeEach block
-        cy.visit(url + "/board?modal-issue-create=true");
+        cy.visit(url + '/board?modal-issue-create=true');
       });
   });
 
-  it("Should create an issue and validate it successfully", () => {
+  it('Should create an issue and validate it successfully', () => {
     // System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       // Type value to description input field
-      cy.get(".ql-editor").type("TEST_DESCRIPTION");
-      cy.get(".ql-editor").should("have.text", "TEST_DESCRIPTION");
+      cy.get('.ql-editor').type('TEST_DESCRIPTION');
+      cy.get('.ql-editor').should('have.text', 'TEST_DESCRIPTION');
 
       // Type value to title input field
       // Order of filling in the fields is first description, then title on purpose
       // Otherwise filling title first sometimes doesn't work due to web page implementation
-      cy.get('input[name="title"]').type("TEST_TITLE");
-      cy.get('input[name="title"]').should("have.value", "TEST_TITLE");
+      cy.get('input[name="title"]').type('TEST_TITLE');
+      cy.get('input[name="title"]').should('have.value', 'TEST_TITLE');
 
       // Open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]')
-        .wait(1000)
-        .trigger("mouseover")
-        .trigger("click");
-      cy.get('[data-testid="icon:story"]').should("be.visible");
+      cy.get('[data-testid="select-option:Story"]').wait(1000).trigger('mouseover').trigger('click');
+      cy.get('[data-testid="icon:story"]').should('be.visible');
 
       // Select Baby Yoda from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
@@ -43,39 +45,39 @@ describe("Issue create", () => {
     });
 
     // Assert that modal window is closed and successful message is visible
-    cy.get('[data-testid="modal:issue-create"]').should("not.exist");
-    cy.contains("Issue has been successfully created.").should("be.visible");
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
 
     // Reload the page to be able to see recently created issue
     // Assert that successful message has dissappeared after the reload
     cy.reload();
-    cy.contains("Issue has been successfully created.").should("not.exist");
+    cy.contains('Issue has been successfully created.').should('not.exist');
 
     // Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog"]')
-      .should("be.visible")
-      .and("have.length", "1")
+      .should('be.visible')
+      .and('have.length', '1')
       .within(() => {
         // Assert that this list contains 5 issues and first element with tag p has specified text
         cy.get('[data-testid="list-issue"]')
-          .should("have.length", "5")
+          .should('have.length', '5')
           .first()
-          .find("p")
-          .contains("TEST_TITLE")
+          .find('p')
+          .contains('TEST_TITLE')
           .siblings()
           .within(() => {
             //Assert that correct avatar and type icon are visible
-            cy.get('[data-testid="avatar:Pickle Rick"]').should("be.visible");
-            cy.get('[data-testid="icon:story"]').should("be.visible");
+            cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
+            cy.get('[data-testid="icon:story"]').should('be.visible');
           });
       });
 
     cy.get('[data-testid="board-list:backlog"]')
-      .contains("TEST_TITLE")
+      .contains('TEST_TITLE')
       .within(() => {
         // Assert that correct avatar and type icon are visible
-        cy.get('[data-testid="avatar:Pickle Rick"]').should("be.visible");
-        cy.get('[data-testid="icon:story"]').should("be.visible");
+        cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
+        cy.get('[data-testid="icon:story"]').should('be.visible');
       });
   });
 
@@ -94,7 +96,8 @@ describe("Issue create", () => {
   });
 });
 
-describe("Issue create", () => {
+// The following blocks should be separate to avoid repeating 'describe' for the same suite
+describe("Issue create additional tests", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.url()
@@ -105,7 +108,7 @@ describe("Issue create", () => {
       });
   });
 
-  it("Test Case 1: Custom Issue Creation", () => {
+  it.only("Test Case 1: Custom Issue Creation", () => {
     // Verify that the modal is present and visible
     cy.get('[data-testid="modal:issue-create"]').should("be.visible");
 
@@ -136,22 +139,9 @@ describe("Issue create", () => {
     // Assert that successful message has disappeared after the reload
     cy.contains("Issue has been successfully created.").should("not.exist");
   });
-});
 
-const faker = require("faker");
-
-describe("Issue create", () => {
-  beforeEach(() => {
-    cy.visit("/");
-    cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project/board`)
-      .then((url) => {
-        // System will already open issue creating modal in beforeEach block
-        cy.visit(url + "/board?modal-issue-create=true");
-      });
-  });
-
-  it("Test Case 2: Random Data Plugin Issue Creation", () => {
+  it.only("Test Case 2: Random Data Plugin Issue Creation", () => {
+    const faker = require("faker");
     const randomTitle = faker.random.word();
     const randomDescription = faker.lorem.words(5);
 

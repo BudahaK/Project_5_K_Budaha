94cb7c539e27883dec98af51a2fe6e221608a46b
BudahaK <buda.kris@gmail.com>
2024-06-07 16:51:40 +0300

    Moved the file.

---
+++ cypress/e2e/e2e-pom/issue-comments_Kristine_W2_S2_A1_V1.cy.js
@@ -0,0 +1,28 @@
+import IssueDetailsPage from "../../pages/issueComment";
+
+describe("Issue comments creating, editing and deleting", () => {
+  const initialComment = "TEST_COMMENT";
+  const editedComment = "TEST_COMMENT_EDITED";
+  const issueDetailsPage = new IssueDetailsPage();
+
+  beforeEach(() => {
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains("This is an issue of type: Task.").click();
+      });
+  });
+
+  it("Should perform comments functionality: add, edit, and delete a comment", () => {
+    //add a comment
+    issueDetailsPage.addComment(initialComment);
+
+    // Edit the added comment
+    issueDetailsPage.editComment(initialComment, editedComment);
+
+    // Delete the edited comment
+    issueDetailsPage.deleteComment(editedComment);
+  });
+});
